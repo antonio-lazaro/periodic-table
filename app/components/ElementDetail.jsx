5ed961ebacf6be710a11c8f158783bcf84b90480
Antonio <antoniolazaro@me.com>
2018-04-17 19:33:10 +0200

    Add more info for ElementDetail

--- app/components/ElementDetail.jsx
+++ app/components/ElementDetail.jsx
@@ -23,6 +23,11 @@ export default class ElementDetail extends React.Component {
         <p className="symbol">{this.props.element.symbol}</p>
         <div style={{ clear: 'both' }}></div>
         <p className="name">{this.props.element.name}</p>
+        <p className="description">{this.props.element.summary}</p>
+        <p className="spec-title first">Atomic number</p>
+        <p className="spec-value">{this.props.element.number}</p>
+        <p className="spec-title">Atomic mass</p>
+        <p className="spec-value">{this.props.element.atomic_mass}</p>
       </div>
     );
   }
